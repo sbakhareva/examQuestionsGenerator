536570817c41e9bdf59bc49de28a2a260c5b5e43
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-01-30 12:18:28 +0300

    начальный коммит

---
+++ src/main/java/org/skypro/generator/ExamQuestionsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionsGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/generator/ExamQuestionsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
